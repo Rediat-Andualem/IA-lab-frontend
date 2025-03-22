a45a3d0b12d0182dd68135ff882944e04c98d45d
RediatAndualem <rediatandualeme@gmail.com>
2025-03-23 00:29:26 +0530

    url updated

--- src/Utility/urlInstance.js
+++ src/Utility/urlInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 let axiosInstance = axios.create({
     baseURL:"https://ia-lab-backend.onrender.com/api"
-    // baseURL:"http://localhost:4789/api"
+    // baseURL:"http://localhost:4789/api" for local
     
 })
 export {axiosInstance}
